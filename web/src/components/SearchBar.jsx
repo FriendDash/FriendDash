9b89567d4aab7e152b68a34b48b204b5451f8c22
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-06-06 23:21:30 -0700

    add icon to searchbar and added chakra ui icons library

--- web/src/components/SearchBar.jsx
+++ web/src/components/SearchBar.jsx
@@ -1,4 +1,12 @@
-import { Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  InputLeftElement,
+  Button,
+} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+
 import React, { useState } from 'react';
 
 // Search Bar Component
@@ -11,13 +19,18 @@ const SearchBar = () => {
   return (
     <div>
       <InputGroup size="md">
+        {' '}
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" />}
+        />
         <Input
           placeholder="Search active orders"
           size="md"
           onChange={e => setSearchQuery(e.target.value)}
         />
         <InputRightElement width="4.5rem">
-          <Button h="100%" size="lg" onClick={handleClick}>
+          <Button h="100%" size="md" onClick={handleClick}>
             Search
           </Button>
         </InputRightElement>{' '}
