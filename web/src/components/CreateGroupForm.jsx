9727b3b65b340f6fccacafe911e8e70cd7ab5c54
StevenZhao318 <stevenzhao318@hotmail.com>
2022-06-10 23:13:03 -0700

    remove <form> we are using react controlled forms, dont need

--- web/src/components/CreateGroupForm.jsx
+++ web/src/components/CreateGroupForm.jsx
@@ -28,46 +28,44 @@ export default chakra(function CreateGroupForm({
       <ModalContent>
         <ModalHeader>Create a Group Order</ModalHeader>
         <ModalBody>
-          <form>
-            <FormControl>
-              <FormLabel>Select Restaurant</FormLabel>
-              <Input
-                type="restaurantName"
-                value={restaurantName}
-                placeholder="Restaurant Name"
-                onChange={e => setRestaurantName(e.target.value)}
-                required
-                marginBottom={'10px'}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Pick Up Time</FormLabel>
-              <input
-                type="time"
-                id="pickUpTimePicker"
-                value={time}
-                onChange={e => setTime(e.target.value)}
-                required
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel marginTop={'10px'}>
-                Max. Number of Group Members
-              </FormLabel>
-              <Select
-                value={groupMembers}
-                onChange={e => setGroupMembers(e.target.value)}
-                required
-                w="100px"
-              >
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </Select>
-            </FormControl>
-          </form>
+          <FormControl>
+            <FormLabel>Select Restaurant</FormLabel>
+            <Input
+              type="restaurantName"
+              value={restaurantName}
+              placeholder="Restaurant Name"
+              onChange={e => setRestaurantName(e.target.value)}
+              required
+              marginBottom={'10px'}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Pick Up Time</FormLabel>
+            <input
+              type="time"
+              id="pickUpTimePicker"
+              value={time}
+              onChange={e => setTime(e.target.value)}
+              required
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel marginTop={'10px'}>
+              Max. Number of Group Members
+            </FormLabel>
+            <Select
+              value={groupMembers}
+              onChange={e => setGroupMembers(e.target.value)}
+              required
+              w="100px"
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </Select>
+          </FormControl>
         </ModalBody>
         <ModalFooter display={'flex'} justifyContent="space-between">
           <Button type="Submit" onClick={() => onCloseCallback()}>
