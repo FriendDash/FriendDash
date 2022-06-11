040fb90e8c365d5fac12dee34aa098004bd28ba6
StevenZhao318 <stevenzhao318@hotmail.com>
2022-06-11 13:52:16 -0700

    add search bar functionality

--- web/src/pages/DashboardPage.jsx
+++ web/src/pages/DashboardPage.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Box, SimpleGrid, VStack } from '@chakra-ui/react';
 import ContentContainer from '../components/ContentContainer';
 import GroupOrderNew from '../components/GroupCard/GroupOrderNew';
@@ -9,21 +10,32 @@ import mockData from '../mockData.json';
 
 // Dashboard Page
 const DashboardPage = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    console.log(searchValue);
+  }, [searchValue]);
+
   return (
     <VStack>
       <Header />
       <Box paddingTop="50px">
         {/* <h1>Dashboard Page</h1> */}
-        <SearchBar />
+        <SearchBar handleChange={setSearchValue} />
         <ContentContainer>
           <SimpleGrid
             columns={{ lg: 3, base: 1 }}
             justifyItems="center"
             spacing="30px"
           >
-            {mockData.groupOrders.map((order, key) => (
-              <GroupOrderCard key={key} data={order} />
-            ))}
+            {mockData.groupOrders.map(
+              (order, key) =>
+                order.restaurant
+                  .toLowerCase()
+                  .includes(searchValue.toLowerCase()) && (
+                  <GroupOrderCard key={key} data={order} />
+                )
+            )}
             <GroupOrderNew />
           </SimpleGrid>
         </ContentContainer>
