bcaf8239cf5f069e55f01f3401f450be144298b9
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-06-20 00:43:51 -0700

    add location

--- web/src/components/CreateGroupForm.jsx
+++ web/src/components/CreateGroupForm.jsx
@@ -11,13 +11,58 @@ import {
   ModalHeader,
   ModalBody,
   ModalFooter,
+  InputRightElement,
+  InputGroup,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default chakra(function CreateGroupForm({ className, isOpen, onClose }) {
   const [restaurantName, setRestaurantName] = useState('');
   const [time, setTime] = useState(new Date());
   const [groupMembers, setGroupMembers] = useState('1');
+  const [pickupLocation, setPickupLocation] = useState('');
+  const [lat, setLat] = useState('');
+  const [long, setLong] = useState('');
+  const [mapsAPIResponse, setMapsAPIResponse] = useState({});
+
+  useEffect(() => {
+    console.log(mapsAPIResponse);
+  }, [mapsAPIResponse]);
+
+  useEffect(() => {
+    /* geolocation is available */
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      console.log(lat + ' - ' + long);
+
+      // console.log(mapsAPIResponse);
+    } else {
+      console.log('geolocation is not supported');
+    }
+  }, []);
+
+  const handleAddress = () => {
+    if (lat !== undefined || long !== undefined)
+      (async () => {
+        const res = await fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+        );
+        const json = await res.json();
+        setMapsAPIResponse(json);
+        setPickupLocation(json.results[0].formatted_address);
+
+        // .then(json => {
+        //   setMapsAPIResponse(json);
+        // })
+        // .then(() => {
+        //   console.log(json);
+        //   setPickupLocation(mapsAPIResponse.results[0].formatted_address);
+        // });
+      })();
+  };
   return (
     <Modal isOpen={isOpen} onClose={onClose} className={className}>
       <ModalOverlay />
@@ -34,8 +79,24 @@ export default chakra(function CreateGroupForm({ className, isOpen, onClose }) {
               required
               marginBottom={'10px'}
             />
-          </FormControl>
-          <FormControl>
+
+            <FormLabel>Select Pickup Location</FormLabel>
+            <InputGroup>
+              <Input
+                type="pickupLocation"
+                value={pickupLocation}
+                placeholder="Address"
+                onChange={e => setPickupLocation(e.target.value)}
+                required
+                marginBottom={'10px'}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleAddress}>
+                  Location
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+
             <FormLabel>Pick Up Time</FormLabel>
             <input
               type="time"
@@ -44,8 +105,7 @@ export default chakra(function CreateGroupForm({ className, isOpen, onClose }) {
               onChange={e => setTime(e.target.value)}
               required
             />
-          </FormControl>
-          <FormControl>
+
             <FormLabel marginTop={'10px'}>
               Max. Number of Group Members
             </FormLabel>
