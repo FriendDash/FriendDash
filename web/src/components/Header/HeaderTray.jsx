5da95e988a137ed77941b44a26b6376d0df13c0d
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-06-23 02:12:49 -0700

    added initial functional google oauth

--- web/src/components/Header/HeaderTray.jsx
+++ web/src/components/Header/HeaderTray.jsx
@@ -16,23 +16,54 @@ import {
 import MainLogo from './../../assets/main-logo.png';
 
 import { HamburgerIcon, CalendarIcon } from '@chakra-ui/icons';
+import { useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
 
 // Button that opens tray when pressed!
 const HeaderTray = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [user, setUser] = useState({});
+
+  // https://developers.google.com/identity/gsi/web/guides/display-button#javascript
+  // https://stackoverflow.com/questions/65234862/how-to-define-variable-google-when-using-google-one-tap-javascript-api
+
+  const handleCredentialResponse = res => {
+    console.log('Encoded JWT ID token: ' + res.credential);
+    let userObject = jwt_decode(res.credential);
+    console.log(userObject);
+    setUser(userObject); // use redux and store this locally
+  };
+
+  useEffect(() => {
+    // global google
+    window.google.accounts.id.initialize({
+      client_id:
+        '635249974359-roi2pf1k7cirel6f972pm15ra4duocbo.apps.googleusercontent.com',
+      callback: handleCredentialResponse,
+    });
+    window.google.accounts.id.renderButton(
+      document.getElementById('signInDiv'),
+      { theme: 'filled_blue', size: 'large', type: 'standard' } // customization attributes
+    );
+    window.google.accounts.id.prompt(); // also display the One Tap dialog
+  }, []);
 
   // We will change this arr to a json arr in future to add urls and corresponding icons
   const menuItems = ['Orders', 'Favorites', 'Payment', 'Help', 'Account'];
+
   return (
     <>
-      <HStack w="100%">
-        <Button colorScheme="gray" onClick={onOpen}>
-          <HamburgerIcon />
-        </Button>
-        <Image src={MainLogo} h="40px" />
-        <Text fontSize="xl" as="b">
-          FriendDash
-        </Text>
+      <HStack w="100%" display="flex" justifyContent="space-between">
+        <HStack>
+          <Button colorScheme="gray" onClick={onOpen}>
+            <HamburgerIcon />
+          </Button>
+          <Image src={MainLogo} h="40px" />
+          <Text fontSize="xl" as="b">
+            FriendDash
+          </Text>
+        </HStack>
+        <div id="signInDiv"></div>
       </HStack>
       <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
@@ -65,10 +96,12 @@ const HeaderTray = () => {
           <DrawerFooter>
             <Box w="100%" p="1px">
               <hr />
-              <HStack p="10px">
+              {/* <HStack p="10px">
                 <CalendarIcon w={6} h={6} />
                 <Text fontSize="20px">Logout</Text>
-              </HStack>
+              </HStack> */}
+
+              {/*  This handles google oauth */}
             </Box>
           </DrawerFooter>
         </DrawerContent>
