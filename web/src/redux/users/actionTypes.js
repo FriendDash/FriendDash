5f7dda28501412a3630d3103f0d9b7ecf869ec47
maggiew7 <maggiewang759@gmail.com>
2022-06-24 21:22:07 -0700

    actionTypes, reducer, and service files for user

---
+++ web/src/redux/users/actionTypes.js
@@ -0,0 +1,6 @@
+export const actionTypes = {
+    GET_USERS: 'users/get',
+    ADD_USER: 'users/add',
+    REMOVE_USER: 'users/remove',
+    UPDATE_USER: 'users/update',
+  };
---
+++ web/src/redux/users/reducer.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = {
+    list: [],
+    getUsers: REQUEST_STATE.IDLE,
+    addUser: REQUEST_STATE.IDLE,
+    removeUser: REQUEST_STATE.IDLE,
+    updateUser: REQUEST_STATE.IDLE,
+    error: null,
+  };
