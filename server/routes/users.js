17adbb0b0754bc8d832c85618a1a0734d9035492
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-07-09 13:06:10 -0700

    added accounts page and deletion route. removed redux usage for storing user data

--- server/routes/users.js
+++ server/routes/users.js
@@ -107,6 +107,7 @@ router.delete('/remove/:userId', function (req, res, next) {
     .catch(err => res.status(404).json('Error: ' + err));
 });
 
+// uses mongodb _id
 router.put('/update/:userId/', function (req, res, next) {
   if (!req.body.userName || !req.body.userEmail || !req.body.userOrders) {
     return res.status(400).send({ message: 'user post req missing info' });
---
+++ web/src/pages/AccountPage.jsx
@@ -0,0 +1,133 @@
+import Header from '../components/Header/Header';
+import {
+  Box,
+  Heading,
+  Avatar,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+  Button,
+  useToast,
+  FormControl,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { removeUserAsync } from '../redux/users/thunk';
+import { useNavigate } from 'react-router-dom';
+import NotFound from '../components/NotFound';
+
+const AccountsPage = () => {
+  const dispatch = useDispatch();
+  const nav = useNavigate();
+  const toast = useToast();
+  const signedOutUserObject = {
+    createdAt: '',
+    googleId: '0',
+    updatedAt: '',
+    userEmail: '',
+    userName: 'Foodie',
+    userOrders: [],
+    userProfile: '',
+    userRating: [],
+  };
+
+  const [user, setUser] = useState(() => {
+    // getting stored value from localStorage
+    const saved = localStorage.getItem('userSession_FriendDash');
+    const initialValue = JSON.parse(saved);
+    return initialValue || signedOutUserObject;
+  });
+
+  const handleDeleteAccount = () => {
+    dispatch(removeUserAsync(user._id));
+    console.log(JSON.stringify(user));
+    handleSignOut();
+    nav('/');
+  };
+
+  const handleSignOut = () => {
+    localStorage.setItem(
+      'userSession_FriendDash',
+      JSON.stringify(signedOutUserObject)
+    );
+    setUser(signedOutUserObject);
+  };
+
+  return (
+    <Box>
+      <Header />
+      <Box p="5%">
+        {user.userName !== 'Foodie' ? (
+          <>
+            <Box
+              marginTop="6%"
+              align="left"
+              p="35px"
+              bg="gray.300"
+              // w={{ lg: '600px', md: '600px', base: '100%' }}
+              w="100%"
+              h="100px"
+              borderRadius="10px"
+            >
+              <Heading size="lg">Profile</Heading>
+            </Box>
+            <Box
+              align="left"
+              p="35px"
+              bg="gray.100"
+              // w={{ lg: '600px', md: '600px', base: '100%' }}
+              w="100%"
+              h="100%"
+              borderRadius="10px"
+            >
+              <FormControl isRequired>
+                <FormLabel htmlFor="Username">Username</FormLabel>
+                <Input
+                  id="Username"
+                  placeholder="Username"
+                  defaultValue={user.userName}
+                />
+                <FormLabel htmlFor="Email">Email</FormLabel>
+                <Input
+                  id="Email"
+                  placeholder="Email"
+                  defaultValue={user.userEmail}
+                />
+              </FormControl>
+
+              {user.userName !== 'Foodie' && (
+                <Box align="right" w="100%" pt="20px">
+                  <Button
+                    onClick={() => {
+                      toast({
+                        position: 'bottom',
+                        title: 'Account deleted.',
+                        description: "We've deleted your account for you.",
+                        status: 'success',
+                        duration: 9000,
+                        isClosable: true,
+                      });
+                      handleDeleteAccount();
+                    }}
+                  >
+                    Delete Account
+                  </Button>
+                </Box>
+              )}
+            </Box>
+          </>
+        ) : (
+          <NotFound element={'User'} />
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default AccountsPage;
