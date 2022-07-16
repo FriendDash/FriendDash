40fd836509b6a487bf1c934e99a21bef7b89bdd2
maggiew7 <maggiewang759@gmail.com>
2022-07-15 23:24:44 -0700

    fix packages, render user's payment methods from stripe

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -3,20 +3,28 @@ const router = express.Router();
 
 const stripe = require('stripe')(process.env.STRIPE_SK);
 
-router.post('/create-checkout-session', async (req, res) => {
+router.post('/:stripeId/create-checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         mode: 'setup',
         payment_method_types: ['card'],
         success_url: 'http://localhost:5000/stripe/success?sessionId={CHECKOUT_SESSION_ID}',
-        cancel_url: 'http://localhost:5000/stripe/cancel',
-        customer: req.body.stripeId
+        cancel_url: 'http://localhost:3000/payment',
+        customer: req.params.stripeId
     });
     res.redirect(session.url);
 });
 
 router.get('/success', async (req, res) => {
-    // const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+    // const session = await stripe.checkout.sessions.retrieve(req.params.session_id);
     res.redirect('http://localhost:3000/payment');
+})
+
+router.get('/paymentMethods/:stripeId', async (req, res) => {
+    const paymentMethods = await stripe.customers.listPaymentMethods(
+        req.params.stripeId,
+        {type: 'card'}
+    );
+    res.send(paymentMethods);
 })
 
 module.exports = router;
--- web/src/pages/PaymentPage.jsx
+++ web/src/pages/PaymentPage.jsx
@@ -1,21 +1,32 @@
 import {
     Box,
     Heading,
-    HStack,
     VStack,
     Image,
     Text,
     Button
 } from '@chakra-ui/react';
 import Header from '../components/Header/Header';
-import {loadStripe} from '@stripe/stripe-js';
-
-const stripePromise = loadStripe('pk_test_51LLIiwFJkD2X0rrAKxtioD3RGqsz0yyNWgJdZVGqkjjjOzGIrRY0E2EjLoLqBCjKj6TqtQ5nyLAZEOKTYF3PpXdO00ClHs67Du');
+import SavedCard from '../components/SavedCard';
+import { useEffect, useState } from 'react';
 
 const PaymentPage = () => {
+    const [paymentMethods, setPaymentMethods] = useState([]);
+    const userStripeId = JSON.parse(localStorage.getItem('userSession_FriendDash')).stripeId;
+    useEffect(() => {
+        async function getPaymentMethods() {
+            const res = await fetch(`http://localhost:5000/stripe/paymentMethods/${userStripeId}`, {
+                method: 'GET',
+            });
+            const json = await res.json();
+            setPaymentMethods(json.data);
+        }
+        getPaymentMethods();
+    }, []);
+
 
     return (
-        <Box>
+        <VStack>
             <Header />
             <Box paddingTop={'50px'}>
                 <VStack>
@@ -23,29 +34,24 @@ const PaymentPage = () => {
                         Your Payment Methods
                     </Heading>
                     <Box borderColor={'black'} borderStyle='solid' borderWidth={'2px'}>
-                        <HStack padding='10px' margin={'10px'} borderColor='gray' borderStyle='solid' borderWidth={'2px'}>
-                            <Image width={'50px'} src='https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/363_Visa_Credit_Card_logo-1024.png' alt='Visa logo'/>
-                            <Text>************1234</Text>
-                        </HStack>
-                        <HStack padding='10px' margin={'10px'} borderColor='gray' borderStyle='solid' borderWidth={'2px'}>
-                            <Image width={'50px'} src='https://cdn3.iconfinder.com/data/icons/payment-method-1/64/_Mastercard-512.png' alt='MasterCard logo'/>
-                            <Text>************5678</Text>
-                        </HStack>
+                        {paymentMethods.map(
+                            paymentMethod => <SavedCard cardType={paymentMethod.card.brand} lastFour={paymentMethod.card.last4} />
+                        )}
                     </Box>
                 </VStack>
                 <VStack>
                     <Heading textAlign='center' my='20px'>
                         Add a Payment Method
                     </Heading>
-                    <form action='http://localhost:5000/stripe/create-checkout-session' method='POST'>
-                        <Button cursor={'pointer'} padding='30px' borderColor='gray' borderStyle='solid' borderWidth={'2px'} type='submit'>
-                            <Image width={'50px'} src='https://cdn3.iconfinder.com/data/icons/leto-finance-money-1/64/credit_card_new_add_plus-256.png' alt='Add credit card'/>
+                    <form action={`http://localhost:5000/stripe/${userStripeId}/create-checkout-session`} method='POST'>
+                        <Button cursor={'pointer'} padding='25px' borderColor='gray' borderStyle='solid' borderWidth={'2px'} type='submit' marginBottom={'50px'}>
+                            <Image width={'50px'} src='https://cdn3.iconfinder.com/data/icons/leto-finance-money-1/64/credit_card_new_add_plus-256.png' alt='Add credit card' />
                             <Text>Add a new credit/debit card</Text>
                         </Button>
                     </form>
                 </VStack>
             </Box>
-        </Box>
+        </VStack>
     );
 };
 
